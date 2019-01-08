8bd2beb58a387c0757042c8b8dc43e74cedca06f
juliely8619 <july8619@yahoo.com>
2019-01-08 10:45:00 -0800

    all methods are called, questions are asked, answers are taken in. it is functioning on a basic level

--- lab01-aboutme/Program.cs
+++ lab01-aboutme/Program.cs
@@ -40,40 +40,72 @@ namespace lab01_aboutme
             if (q1SiblingsGuessInt != q1Answer)
             {
                 Console.WriteLine("Good guess");
-            }
-            return q1Answer;
+                return q1Answer;
+            } else 
+                return q1Answer;
         }
 
         //question two, return string
         private static string q2Color()
         {
             Console.WriteLine("What is my favorite color?");
-            string q2Answer = "Pink";
-            return q2Answer;
+            string q2ColorGuess = Console.ReadLine();
+            string q2ColorGuessCap = q2ColorGuess.ToUpper();
+            string q2Answer = "PINK";
+            if (q2ColorGuessCap != q2Answer)
+            {
+                Console.WriteLine("Good guess");
+                return q2Answer;
+            }
+            else
+                return q2Answer;
         }
 
         //question three, return boolean
         private static bool q3Foodie()
         {
             Console.WriteLine("True or false: I am a foodie");
+            string q3FoodieGuess = Console.ReadLine();
+            bool q3FoodieGuessBool = Convert.ToBoolean(q3FoodieGuess);
             bool q3Answer = true;
-            return q3Answer;
+            if (q3FoodieGuessBool != q3Answer)
+            {
+                Console.WriteLine("Good guess");
+                return q3Answer;
+            }
+            else
+                return q3Answer;
         }
 
         //question four, return int
         private static int q4FavNum()
         {
             Console.WriteLine("What is my favorite Number?");
+            string q4FavNumGuess = Console.ReadLine();
+            int q4FavNumGuessInt = Convert.ToInt32(q4FavNumGuess);
             int q4Answer = 36;
-            return q4Answer;
+            if (q4FavNumGuessInt != q4Answer)
+            {
+                Console.WriteLine("Good guess");
+                return q4Answer;
+            } else
+                return q4Answer;
         }
 
         //question five, return string
         private static string q5Name()
         {
             Console.WriteLine("What is my current favorite name?");
-            string q5Answer = "Jasper";
-            return q5Answer;
+            string q5NameGuess = Console.ReadLine();
+            string q5NameGuessCap = q5NameGuess.ToUpper();
+            string q5Answer = "JASPER";
+            if (q5NameGuessCap != q5Answer)
+            {
+                Console.WriteLine("Good guess");
+                return q5Answer;
+            }
+            else
+                return q5Answer;
         }
     }
 }
