1d401e61e99a030fe97525c2843c5dd4deca699a
juliely8619 <july8619@yahoo.com>
2019-01-08 10:05:04 -0800

    chose my questions

--- lab01-aboutme/Program.cs
+++ lab01-aboutme/Program.cs
@@ -6,18 +6,43 @@ namespace lab01_aboutme
     {
         static void Main(string[] args)
         {
-            
+
             //calls methods for each question
+            q1Siblings();
+            q2Color();
+            q3Foodie();
+            q4FavNum();
+            q5Name();
         }
 
         //question one, return int
+        private static int q1Siblings()
+        {
+            Console.WriteLine("How many siblings do I have?");
+        }
 
         //question two, return string
+        private static string q2Color()
+        {
+            Console.WriteLine("What is my favorite color?");
+        }
 
         //question three, return boolean
+        private static bool q3Foodie()
+        {
+            Console.WriteLine("True or false: I am a foodie");
+        }
 
-        //question four, return void
+        //question four, return int
+        private static int q4FavNum()
+        {
+            Console.WriteLine("What is my favorite Number?");
+        }
 
         //question five, return string
+        private static string q5Name()
+        {
+            Console.WriteLine("What is my current favorite name?");
+        }
     }
 }
